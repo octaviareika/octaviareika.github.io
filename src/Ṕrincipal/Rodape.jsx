be0e467438493b2c132cae94780195c8dd0cd687
octaviareika <ceciliaperet81@gmail.com>
2024-04-20 21:06:19 -0300

    oi

---
+++ src/Ṕrincipal/Rodape.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Rodape.css";
+export default function Rodape(props){
+    return (
+    
+        <footer className="rodapeInicio">
+            <ul className="rodape-sobre">
+                <h3 className="linha-rodape titulo">Habilidades</h3>
+                <li className="linha-rodape">Boa relação interpessoal</li>
+                <li  className="linha-rodape">Gosta de aprender</li>
+                <li  className="linha-rodape">Comunicativa</li>
+                <li  className="linha-rodape">Ajuda na resolução de problemas</li>
+                <li  className="linha-rodape">Proativa</li>
+            </ul>
+        </footer>
+    
+    )
+
+}
