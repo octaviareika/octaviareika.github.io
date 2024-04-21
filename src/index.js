7baf62614e31831bab9e6cc2addd4f0a6f7edeb2
octaviareika <ceciliaperet81@gmail.com>
2024-04-20 21:12:36 -0300

    oi

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Header from './portfolio/src/Ṕrincipal/Header';
-import {Router, Routes, Route} from 'react-router';
-import App from './portfolio/src/App';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
