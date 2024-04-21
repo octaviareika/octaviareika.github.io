5f0b1024c5ad997e12ba30b4129830630756ba13
octaviareika <ceciliaperet81@gmail.com>
2024-04-20 21:04:00 -0300

    oi

--- README.md
+++
@@ -1,2 +0,0 @@
-# octaviareika.github.io
-Sobre mim 
---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Header from './portfolio/src/Ṕrincipal/Header';
+import {Router, Routes, Route} from 'react-router';
+import App from './portfolio/src/App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+    
+);
+
